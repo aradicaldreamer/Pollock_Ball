2ffa39ba0fd2dd9671c38b560c4a67a45f0203c3
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2017-12-17 16:48:14 +0000

    Changes to collisions

---
+++ Pollock_Ball/Assets/BallController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Called when the ball collides with objects. Should print decals and set new vector
+    private void OnCollisionEnter(Collision collision)
+    {
+        // to print decals on contact
+
+        if (collision.rigidbody) {
+            collision.rigidbody.AddForce(Vector3.up * 15);
+        }
+    }
+
+}
