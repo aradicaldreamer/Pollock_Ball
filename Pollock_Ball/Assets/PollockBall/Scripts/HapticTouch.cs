ed13839fb53829407eef3bb59510149b7ea2c187
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2017-12-18 22:19:28 +0000

    New Controller Scripts

---
+++ Pollock_Ball/Assets/PollockBall/Scripts/HapticTouch.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HapticTouch : MonoBehaviour {
+
+	public bool hapticFlag = false;
+	private SteamVR_TrackedObject trackedObject;
+	private SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		trackedObject = GetComponent<SteamVR_TrackedObject> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObject.index);
+		if (hapticFlag) {
+			device.TriggerHapticPulse (500);
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		hapticFlag = true;
+	}
+
+	void OnCollisionExit()
+	{
+		hapticFlag = false;
+	}
+}
